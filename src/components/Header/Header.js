f3fed63a7a0903a178ae0dbb138f3ae934a84b6d
Laura Gonzalez <lauragonaguirre@gmail.com>
2022-06-07 20:05:42 -0600

    added readme

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import Navbar from "../Navbar/Navbar";
 export default function Header() {
     return (
         <header>
-            <h1 className="title">Your Breed Finder</h1>
+            <h1 className="title">Your Dog Breed Finder</h1>
             <Navbar />
         </header>
     );
